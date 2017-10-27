b47a86be1dd26575759fa4b1c4b622904373fcba
NSN <nisanlevy666@gmail.com>
2017-10-27 12:40:36 +0200

    commit again after unsaved edited files

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function a() {
-
+  wp_enqueue_script( 'angular',get_template_directory_uri() );
 }
 
-
+add_action( 'wp_enqueue_scripts', $function_to_add, $priority = 10, $accepted_args = 1 )
  ?>
