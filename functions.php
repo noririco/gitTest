8acaa2feb4eabc042e684828cb010fce921b5da4
NSN <nisanlevy666@gmail.com>
2017-10-27 15:33:28 +0200

    adding more extentions to Atom , adding more content in index.html, added main styles.css file

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 function a() {
   wp_enqueue_script( 'angular',get_template_directory_uri() );
 }
-
+//TODO: Example using todo plugin ctrl k ctrl t
 add_action( 'wp_enqueue_scripts', $function_to_add, $priority = 10, $accepted_args = 1 )
  ?>
